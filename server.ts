f587c6d6b826e801686796f375b8ac9a99f3e0b7
reflexglo <theicycrew@gmail.com>
2022-03-28 16:01:39 -0400

    rewr

--- server.ts
+++ server.ts
@@ -3,6 +3,9 @@ import ejs from 'ejs';
 import {Schema, model, connect} from 'mongoose';
 const mongoose = require('mongoose');
 const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 mongoose.connect('mongodb+srv://admin:password1234@cluster0.gj8tf.mongodb.net/MapData?retryWrites=true&w=majority');
 
@@ -28,11 +31,15 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/add', function (req, res) {
+app.post('/add', function (req, res) {
+    console.log(req.body);
+    var name = req.body.name;
+    var capital = req.body.capital;
+    var leader = req.body.leader;
     const newRegion = new region({
-        name: "New Test",
-        capital: "New Test",
-        leader: "New Test"
+        name: name,
+        capital: capital,
+        leader: leader
     })
     newRegion.save();
   });
